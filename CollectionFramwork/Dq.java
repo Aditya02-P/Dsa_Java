baa6f130ed43e83e7575bb417c0afe9be866124c
Aditya02-P <pradhanaditya0725@gmail.com>
2026-04-11 23:03:16 +0530

    Added learnings

---
+++ CollectionFramwork/Dq.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Dq {
+    public static void main(String[] args) {
+       ArrayDeque<Integer> dq = new ArrayDeque<>();
+       dq.push(1);
+       dq.push(2);
+       dq.push(3);
+        System.out.println(dq.pop());
+       Deque<Integer> dq2 = new ArrayDeque<>();
+       dq2.push(1);
+       PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+
+
+    }
+}
